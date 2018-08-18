2fe6266189ca3f0b7e1c1c8f7fb66e1af2bc7c7a
Yang Liu <foamliu@yeah.net>
2018-08-18 11:21:45 +0800

    update

--- config.py
+++ config.py
@@ -31,9 +31,9 @@ batch_size = 32
 num_epochs = 1000
 patience = 50
 
-lambda_coord = 5.0
-lambda_obj = 1.0
-lambda_noobj = 0.5
+lambda_coord = 1.0
+lambda_obj = 5.0
+lambda_noobj = 1.5
 lambda_class = 1.0
 
-max_boxes = 10  # integer, maximum number of predicted boxes in an image
+max_boxes = 50  # integer, maximum number of predicted boxes in an image
